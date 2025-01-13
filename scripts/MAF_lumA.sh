ee6c98dc5d32eaf7f7ccde5a51033f9da9fa7472
annamelidi <89490095+annamelidi@users.noreply.github.com>
2025-01-13 18:37:47 +0100

    Delete scripts directory

--- scripts/MAF_lumA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate Python Virtual Environment
-#. /usr/local/envs/py37/bin/activate
-module load python/3.10
-# Define the command you want to run
-python3 tf_maf.py ../raw_data/trrust_rawdata.human.tsv ../raw_data/mutations.csv
-
-
